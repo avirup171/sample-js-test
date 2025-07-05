59b0af886c14e1f2715616db3b328ce44ef3b3dd
Avirup Basu <ami.avirup17@gmail.com>
2025-07-05 09:04:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 // This is a test file for character analysis
-const name = "Renée";         // é is a special character
-const currency = "€100";      // € is a special character
-const emoji = "Hello 👋";     // 👋 is a special character (Unicode)
-const chinese = "你好";         // 你好 means "hello" in Chinese
-const normal = "Hello World"; // no special characters
+
 
 function greet() {
 
